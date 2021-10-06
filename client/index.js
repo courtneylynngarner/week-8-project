a99969829e285731ee51477d2e511c27c6da7b22
Courtney Garner <courtneylynn108@gmail.com>
2021-10-06 06:57:32 -0600

    Stores and sends data to the front end

--- client/index.js
+++ client/index.js
@@ -1,14 +1,39 @@
 const menu = document.querySelector(".hamburger");
 const sidebar = document.querySelector(".sidebar-div");
 const submitBtn = document.querySelector("button");
-const newsletterForm = document.querySelector("form");
-const emailInput = document.querySelector("input");
-
+const newsletterForm = document.querySelector(".newsletter");
+const emailInput = document.getElementById("email-address");
+const newsletterResponse = document.querySelector(".newsletter-response");
+const formControl = document.querySelector(".form-control");
+const participate = document.getElementById("participate");
+const newsletterEmail = document.getElementById("newsletter-email");
+const userEmail = document.getElementById("user-email");
+console.log(userEmail);
+console.log(newsletterResponse);
 menu.addEventListener("click", () => {
   sidebar.classList.toggle("show-menu");
 });
 
+participate.addEventListener("click", () => {
+  formControl.classList.toggle("show-message");
+});
+
 newsletterForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(emailInput.value);
+  console.log("I'm here");
+  console.log(newsletterEmail.value);
+  const newsletterEmailSubmission = newsletterEmail.value;
+  console.log(newsletterEmailSubmission);
+  axios
+    .post(`http://localhost:4000/api/newsletter-sign-up`, {
+      newsletterEmailSubmission,
+    })
+    .then((res) => {
+      console.log(res.data);
+      const resArray = res.data.split(" ");
+      console.log(resArray);
+      userEmail.textContent = `${resArray[0]}`;
+
+      newsletterResponse.style.visibility = "unset";
+    });
 });
--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,15 @@
-function newsletterSignUp() {
+let newsletterEmailArray = [];
+
+function newsletterSignUp(req, res) {
   console.log("we done got here");
+  console.log(req.body);
+  const { newsletterEmailSubmission } = req.body;
+  console.log(newsletterEmailSubmission);
+  newsletterEmailArray.push(newsletterEmailSubmission);
+  console.log(newsletterEmailArray);
+  res
+    .status(200)
+    .send(`${newsletterEmailSubmission} has successfully been submitted.`);
 }
 
-module.exports = {};
+module.exports = { newsletterSignUp };
